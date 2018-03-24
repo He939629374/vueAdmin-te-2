2e5bd4bd35140afda6b409d352778b280292abfa
He939629374 <32150775+He939629374@users.noreply.github.com>
2018-03-24 12:10:26 +0800

    答题

---
+++ src/components/Options.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+     <div id='logo'>{{logo.lo}}(我是父组件传递过来的值)</div> 
+      <div v-for="(item,index) in list" :key='index' >
+        <el-button v-if="item.edit" @click="item.edit = !item.edit" v-model="item.title">edit</el-button>
+        <el-button v-else @click="item.edit = !item.edit">ok</el-button>
+        <el-input v-if="!item.edit" class="item.edit-input" size="small" v-model="item.title"></el-input>
+        <p v-else @click="item.edit = false" >{{ item.title }}</p>
+        <el-radio v-model="item.radio" label="1">是</el-radio>
+        <el-radio v-model="item.radio" label="0">否</el-radio>
+      </div>
+      
+      <div v-for="(item,index) in lis" :key='index'>
+        <p >{{ item.title }}</p>
+        <el-rate
+        v-model="item.value4"
+        :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
+        show-text>
+        </el-rate>
+      </div>
+
+      <div v-for="(item,index) in checkQuery" :key='index'>
+        <p >{{ item.title }}</p>
+        <div>
+          <el-checkbox v-for="mpb in checkQuery[index].check" :key="mpb.id" v-model="mpb.checked">{{mpb}}</el-checkbox>
+        </div>
+      </div>
+      
+      <div v-for="(item,index) in AskQuery" :key='index'>
+        <p >{{ item.title }}</p>
+        <el-input
+          type="textarea"
+          autosize
+          placeholder="请输入内容"
+          v-model="item.input10"
+          clearable>
+        </el-input>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    props: ['logo', 'list', 'lis', 'checkQuery', 'AskQuery'],
+    methods: {
+      handleCreate() {
+      },
+      confirmedit(index) {
+        this.item[index].edit = false
+      }
+    }
+  }
+</script>
+<style>
+.el-input--small .el-input__inner {
+    height: 50px;
+}
+.filter-container1{
+  width: 500px;
+  margin: auto;
+}
+#CreatUl{
+    width: 800px;
+    background: #0095da;
+    padding: 6px auto;
+
+}
+#CreatUl li{
+    /* width: 160px;
+    display: inline; */
+    border-right: 1px solid #0072bc;
+}
+#CreatUl li a{
+    font-size: 16px;
+    line-height: 36px;
+    margin: 10px 10px;
+}
+a:hover{
+    color: #f58220;
+    background: #FFF;
+    border-radius: 4px;
+    margin: 0px;
+}
+
+</style>
--- src/views/table/work.vue
+++ src/views/table/work.vue
@@ -2,7 +2,7 @@
   <div class="app-container ">
     <div class="filter-container" style="padding-bottom:10px">
         <h2>此处标题</h2>
-    <div class="filter-container" style="padding-bottom:10px">
+    <div class="filter-container1" style="padding-bottom:10px">
       <el-select clearable style="width: 110px" class="filter-item" v-model="headOptions.type" placeholder="类型">
         <el-option v-for="item in typeOptions" :key="item" :label="item" :value="item" >
         </el-option>
@@ -12,85 +12,99 @@
         </el-option>
       </el-select>
       <el-button class="filter-item" type="primary"  icon="el-icon-search" @click="handleCreate">新增</el-button>
-      <div v-for="(item,index) in listQuery" :key='index'>
-        <p >{{ item.title }}</p>
-        <el-radio v-model="item.radio" label="1">是</el-radio>
-        <el-radio v-model="item.radio" label="0">否</el-radio>
-      </div>
-      <div v-for="(item,index) in listQuery" :key='index'>
-        <p >{{ item.title }}</p>
-        <el-rate
-        v-model="value4"
-        :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
-        show-text>
-        </el-rate>
-      </div>
-      <div v-for="(item,index) in checkQuery" :key='index'>
-        <p >{{ item.title }}</p>
-      </div>
-      <div>
-        <el-checkbox-group v-for="(item,index) in checkQuery" :key='index'>
-        <el-checkbox :label="item.checkList[index]"></el-checkbox>
-        </el-checkbox-group>
-      </div>
+      <myQ :list="listQuery2" :logo="logoMsg"  :lis="liswork" :checkQuery="checkwork" :AskQuery="Askwork">
+
+      </myQ>
     </div>
     </div>
   </div>
 </template>
 
 <script>
+  import myQ from '@/components/Options.vue'
   export default {
     data() {
       return {
+        logoMsg: { lo: '21' },
         typeOptions: ['单选', '多选', '填空', '上传'],
         statusOptions: ['是否', '星级'],
         headOptions: {
           type: '',
           status: ''
         },
-        listQuery: [
+        listQuery2: [
           {
-            title: 'asd',
-            radio: '1'
+            title: 'asdd',
+            radio: '1',
+            edit: true
+          },
+          {
+            title: 'asdd2',
+            radio: '0',
+            edit: true
           }
         ],
-        value4: '',
-        checkQuery: [
-        {
-          title:'asddas',
-          checkList: ['a','b','c']
-        }],
-        
+        liswork: [
+          {
+            title: 'asdd',
+            radio: '1',
+            edit: true,
+            value4: ''
+          },
+          {
+            title: 'asdd2',
+            radio: '0',
+            edit: true,
+            value4: ''
+          }
+        ],
+        checkwork: [
+          {
+            id: 1,
+            checked: false,
+            title: 'asddas',
+            check: ['上海', '北京', '广州', '深圳']
+          },
+          {
+            id: 1,
+            checked: false,
+            title: 'adss',
+            check: ['上海1', '北京1', '广州1', '深圳1']
+          }
+        ],
+        Askwork: [
+          {
+            title: 'asd112',
+            input10: ''
+          },
+          {
+            title: 'eqd12',
+            input10: ''
+          }
+        ]
       }
     },
+    components: { myQ },
     methods: {
       handleCreate() {
+        if(this.headOptions.type == '单选'){
+          this.listQuery2.push({ title: 'moren', radio: '', edit: true })
+        }
+        else if(this.headOptions.type == '多选'){
+          this.checkQuery.push({ title: 'moren', radio: '', edit: true })
+        }
+        else if(this.headOptions.type == '填空'){
+          this.AskQuery.push({ title: 'moren', radio: '', edit: true })
+        }
+        else if(this.headOptions.type == '上传'){
+          
+        }
+        this.logoMsg.lo = '3'
+        // alert(this.listQuery2[1].radio)
       }
     }
-  }
+}
 </script>
 <style>
-#CreatUl{
-    width: 800px;
-    background: #0095da;
-    padding: 6px auto;
-
-}
-#CreatUl li{
-    width: 160px;
-    display: inline;
-    border-right: 1px solid #0072bc;
-}
-#CreatUl li a{
-    font-size: 16px;
-    line-height: 36px;
-    margin: 10px 10px;
-}
-a:hover{
-    color: #f58220;
-    background: #FFF;
-    border-radius: 4px;
-    margin: 0px;
-}
 
 </style>
